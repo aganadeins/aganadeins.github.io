fb4ea13b086923e0e98b1d0439551454e9986cb0
aganadeins <agatanadeina@gmail.com>
2020-11-07 08:09:30 +0300

    7nov

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let burgerBtn = document.getElementById ('burger-btn');
-burgerBtn.onclick = function showMenu() {
-    if (document.getElementById("burger-menu-inside").style.display == none) {
-        document.getElementById("burger-menu-inside").style.display = block;
-    }
-    else {
-        document.getElementById("burger-menu-inside").style.display = none;   
-    }
-};
